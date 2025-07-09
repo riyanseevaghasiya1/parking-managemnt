976bac4db4a7691779110db5af03994f2573b4a3
riyanseevaghasiya1 <riyansee1.kalathiyainfotech@gmail.com>
2025-07-09 17:51:59 +0530

    riyansee09-07

--- src/components/LogoutModal.js
+++ src/components/LogoutModal.js
@@ -7,24 +7,24 @@ const LogoutModal = ({ isOpen, onClose, onLogout }) => {
   return (
     <div className="logout-modal-overlay" onClick={onClose}>
       <div className="logout-modal-content" onClick={e => e.stopPropagation()}>
-        <div  className='mb-3'>
+        <div  className='text-end'>
         <button className="close-button" onClick={onClose}>×</button>
         </div>
-        <div className="r_title">
+        <div className='p-3'>
+          <div className="r_title">
           <p className='mb-2' style={{fontSize:'20px'}}>Logout</p>
         </div>
-        
         <div className="logout-modal-body">
           <p className='text-center'>Are you sure to logout?</p>
         </div>
-
-        <div className="logout-modal-footer mb-3">
+        <div className="logout-modal-footer mt-4 mb-4">
           <button className="cancel-button" onClick={onClose}>
             Cancel
           </button>
           <button className="confirm-button" onClick={onLogout}>
             Yes
           </button>
+        </div>
         </div>
       </div>
     </div>
